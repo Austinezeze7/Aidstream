bb6cd91b470549becaccc5b00d310e4199ba96f8
Eddy-Odero <odiwuorodero8948@gmain.com>
2026-05-28 19:00:00 +0300

    updating backend

--- backend/main.go
+++ backend/main.go
@@ -1,22 +1,46 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"log"
+
+	"Aidstream/backend/utils"
 )
 
 func main() {
-	fileServer := http.FileServer(http.Dir("../frontend"))
-	http.Handle("/", fileServer)
 
+	err := utils.ConnectDB()
+	if err != nil {
+		log.Fatal("Database connection failed:", err)
+	}
+
+	fileServer := http.FileServer(http.Dir("../frontend"))
+	http.Handle("/static/", http.StripPrefix("/static/", fileServer))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "../frontend/html/signin.html")
+	})
 
 	http.HandleFunc("/api/donate", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
-		fmt.Fprint(w, `{"status": "success", "message": "Backend connected successfully!"}`)
+
+		if r.Method != http.MethodPost {
+			http.Error(w, "Only POST allowed", http.StatusMethodNotAllowed)
+			return
+		}
+
+		response := map[string]string{
+			"status":  "success",
+			"message": "Backend connected successfully!",
+		}
+
+		json.NewEncoder(w).Encode(response)
 	})
 
 	fmt.Println("Server running at: http://localhost:8080")
-	err := http.ListenAndServe(":8080", nil)
+
+	err = http.ListenAndServe(":8080", nil)
 	if err != nil {
 		panic(err)
 	}
